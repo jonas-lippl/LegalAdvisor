050f6814700a96898fb5a7bf79101e4f41441ed0
Jonas Lippl <jonas.lippl@tngtech.com>
2022-07-08 11:03:26 +0200

    adding admin mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,62 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Node } from './logic/Node'
-import {Option} from "./Option";
+import { Node } from './logic/Node';
+import EditIcon from '@mui/icons-material/Edit';
+import CloseIcon from '@mui/icons-material/Close';
+import { Option } from './Option';
+import { Fab } from '@mui/material';
 
 const App = () => {
-  const nextNodeA = new Node("Question A", { "date": new Node("", {}) });
-  const nextNodeB = new Node("Question B", { "Not lol": new Node("", {})});
+  const [adminMode, setAdminMode] = useState<boolean>(false);
+  const nextNodeA = new Node('Question A', { date: new Node('', {}) });
+  const nextNodeB = new Node('Question B', { 'Not lol': new Node('', {}) });
 
   const answerOptions = {
-    "Yes": nextNodeA,
-    "No": nextNodeB
+    Yes: nextNodeA,
+    No: nextNodeB,
   };
-  const defaultNode = new Node("Question", answerOptions);
+  const defaultNode = new Node('Question', answerOptions);
 
-  const [currentNode, setCurrentNode] = useState(defaultNode)
+  const [currentNode, setCurrentNode] = useState(defaultNode);
 
-  const options = Object.keys(currentNode.options).map(key =>
-      <Option onClick={() => setCurrentNode(currentNode.options[key])} text={key}/>)
+  const options = Object.keys(currentNode.options).map((key) => (
+    <Option
+      onClick={() => setCurrentNode(currentNode.options[key])}
+      text={key}
+    />
+  ));
 
+  if (adminMode) {
+    return (
+      <>
+        <h1>LegalAdvisor</h1>
+        <div className="header">
+          <h2>Admin Mode</h2>
+          <Fab
+            color="primary"
+            size={'small'}
+            onClick={() => setAdminMode(false)}
+          >
+            <CloseIcon />
+          </Fab>
+        </div>
+      </>
+    );
+  }
   return (
     <>
-      <h1>{currentNode.question}</h1>
+      <h1>LegalAdvisor</h1>
+      <div className="header">
+        <h2>{currentNode.question}</h2>
+        <div className={'adminModeButton'}>
+          <Fab color="primary" size="small" onClick={() => setAdminMode(true)}>
+            <EditIcon />
+          </Fab>
+        </div>
+      </div>
       {options}
     </>
   );
-}
+};
 
 export default App;
--- src/Option.tsx
+++ src/Option.tsx
@@ -1,32 +1,36 @@
-import React, {useState} from 'react';
-import {Button, TextField} from "@mui/material";
+import React, { useState } from 'react';
+import { Button, TextField } from '@mui/material';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-
-interface OptionProps{
-    text: string;
-    onClick: ()=>void;
+interface OptionProps {
+  text: string;
+  onClick: () => void;
 }
 
-export const Option: React.FC <OptionProps>=(props)=>{
-    const [date, setDate]= useState<Date|null>(null)
-    const currentDate = new Date()
-    const [dateDifference, setDateDifference] = useState<number>(0)
+export const Option: React.FC<OptionProps> = (props) => {
+  const [date, setDate] = useState<Date | null>(null);
 
-    if (props.text.includes("date")){
-        return(<LocalizationProvider dateAdapter={AdapterDateFns}><DatePicker
-            label="Date of dismissal"
-            value={date}
-            onChange={(newValue) => {
-                setDate(newValue);
-                setDateDifference(Date.now() - +(newValue!))
-            }}
-            renderInput={(params) => <TextField {...params} />}
-        /></LocalizationProvider>)
-    }
-    return(
-        <Button onClick={()=>props.onClick()} variant='outlined'>{props.text}</Button>
+  if (props.text.includes('date')) {
+    return (
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DatePicker
+          label="Date of dismissal"
+          value={date}
+          onChange={(newValue) => {
+            setDate(newValue);
+            if (Date.now() - Number(newValue!) > 21) {
+            }
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
     );
-}
+  }
+  return (
+    <Button onClick={() => props.onClick()} variant="outlined">
+      {props.text}
+    </Button>
+  );
+};
