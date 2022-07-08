05705e16488bde637d5377d49ae3db29afb8afd5
Ondřej Merkun <ondrejmerkun@microsoft.com>
2022-07-08 22:40:40 +0200

    Changes

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="LegalAdvisor">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin" element={<Admin />} />
